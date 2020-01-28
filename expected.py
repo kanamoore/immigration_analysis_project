0f54ea364a6db0ab825c6ad40e6124227f4b589b
Stacy <stacy@altmetric.com>
2020-01-27 19:36:23 -0600

    added expected values calculation

---
+++ expected.py
@@ -0,0 +1,7 @@
+def expected(year_total, category, pop_size):
+    a = category / pop_size
+    b = year_total / pop_size
+    
+    expect_val = a * b * pop_size
+    
+    return round(expect_val, 3)
