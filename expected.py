5ed4d55e1c224a1bf4b9ddd6e826f37293f49f53
Stacy <stacy@altmetric.com>
2020-01-27 20:21:08 -0600

    chi-square test code integrated

--- expected.py
+++ expected.py
@@ -2,6 +2,6 @@ def expected(year_total, category, pop_size):
     a = category / pop_size
     b = year_total / pop_size
     
-    expect_val = a * b * pop_size
+    expect_val = (a * b) * pop_size
     
     return round(expect_val, 3)
